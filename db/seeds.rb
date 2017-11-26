d56ab14460ac7f40bfea648d0c75d639dfd3cc58
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-26 10:50:20 +0200

    5.8 final mission

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(:name => "Product 4", :description => "New", :image_url => "Product4.jpg", :colour => "blue", :price => 50)
